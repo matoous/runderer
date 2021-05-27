44c2427f59a132f43b5ef2b3cf15a655643964d0
Matouš Dzivjak <matousdzivjak@gmail.com>
2021-05-27 21:23:42 +0200

    feat(v0.0.3): draw_line fourth attempt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use image::{ImageBuffer, Pixel};
+use image::imageops::{flip_vertical_in_place};
 use std::path::Path;
 use std::ops::{DerefMut};
 use core::mem;
@@ -16,20 +17,33 @@ fn draw_line<P: 'static + Pixel, Container: DerefMut<Target=[P::Subpixel]>>(mut 
         mem::swap(&mut x0, &mut x1);
         mem::swap(&mut y0, &mut y1);
     }
+    let dx = (x1 - x0) as f32;
+    let dy = (y1 - y0) as f32;
+    // how much further from the real line will we get in each iteration
+    let derror = (dy / dx).abs();
+    let mut error = 0.;
+    let mut y = y0;
     for x in x0..x1 {
-        let dx = (x - x0) as f32 / (x1 - x0) as f32;
-        let y = y0 as f32 * 1. - dx + y1 as f32 * dx;
         match steep {
             true => img.put_pixel(y as u32, x, pixel),
             false => img.put_pixel(x, y as u32, pixel)
+        }
+        error += derror;
+        if error > 0.5 {
+            if y1 > y0 {
+                y += 1;
+            } else {
+                y -= 1;
+            }
+            error -= 1.;
         }
     }
 }
 
 fn main() {
     let white = image::Rgb([255, 255, 255]);
-    let red = image::Rgb([255, 0, 0]);
-    let mut image = image::ImageBuffer::new(100, 100);
+    let red = image::Rgb([255 , 0, 0]);
+    let mut image: ImageBuffer<image::Rgb<u8>, Vec<u8>> = ImageBuffer::new(100, 100);
     for x in 0..image.width() {
         for y in 0..image.height() {
             image.put_pixel(x, y, image::Rgb([0, 0, 0]))
@@ -38,5 +52,6 @@ fn main() {
     draw_line(13, 20, 80, 40, &mut image, white); // gets over-drawn by the next red line
     draw_line(20, 13, 40, 80, &mut image, red);
     draw_line(80, 40, 13, 20, &mut image, red);
+    flip_vertical_in_place(&mut image);
     image.save(&Path::new("image.png")).unwrap();
 }
