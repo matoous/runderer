8a644c8804c450b68d2514c0db63cf389bc2a147
Matouš Dzivjak <matousdzivjak@gmail.com>
2021-05-28 15:56:38 +0200

    feat(v0.1.3): draw_triangle - render the head with shading

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 My attempt at [Tiny Render](https://github.com/ssloy/tinyrenderer) but instead of C++ written in Rust.
 
-## [Part 1](https://github.com/ssloy/tinyrenderer/wiki/Lesson-1:-Bresenham%E2%80%99s-Line-Drawing-Algorithm)
+## [Part 1](https://github.com/ssloy/tinyrenderer/wiki/Lesson-1:-Bresenham%E2%80%99s-Line-Drawing-Algorithm) - Line Drawing Algorithm
 
-![african head](african_head.png)
+![african head](african_head_mesh.png)
 
 Drawing lines using [Bresenham's line algorithm](https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm),
 loading [Wavefront .obj](https://en.wikipedia.org/wiki/Wavefront_.obj_file) files, rendering simple mesh.
@@ -14,3 +14,10 @@ to make everything work in rust and finally decided to go with [tobj](https://do
 instead of writing my own.
 
 Some more ideas for things to render later on: https://github.com/tinyobjloader/tinyobjloader
+
+## [Part 2](https://github.com/ssloy/tinyrenderer/wiki/Lesson-2:-Triangle-rasterization-and-back-face-culling) - Triangle Rasterization
+
+![african head](african_head.png)
+
+Triangle rasterization using [Standard Algorithm](http://www.sunshine2k.de/coding/java/TriangleRasterization/TriangleRasterization.html#algo2),
+triangle rasterization using [Bresenham Algorithm](http://www.sunshine2k.de/coding/java/TriangleRasterization/TriangleRasterization.html#algo3),
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,133 @@
-use image::{ImageBuffer, Pixel};
+use image::{ImageBuffer, Pixel, Primitive};
 use image::imageops::{flip_vertical_in_place};
 use std::path::Path;
 use std::ops::{DerefMut};
 use core::mem;
-use nalgebra::{Vector2};
+use nalgebra::{Vector2, Vector3};
 use tobj;
+use std::cmp::{min, max};
+use rand::Rng;
 
+#[derive(Debug)]
+pub struct Triangle(pub Vector2<i32>, pub Vector2<i32>, pub Vector2<i32>);
 
-// TODO instead of u32 use generic
-fn draw_triangle<P: 'static + Pixel, Container: DerefMut<Target=[P::Subpixel]>>
-(mut p0: Vector2<i32>, mut p1: Vector2<i32>, mut p2: Vector2<i32>, img: &mut ImageBuffer<P, Container>, pixel: P) {
-    // sort by y coordinate
-    if p0.y > p1.y { mem::swap(&mut p0, &mut p1); }
-    if p0.y > p2.y { mem::swap(&mut p0, &mut p2); }
-    if p1.y > p2.y { mem::swap(&mut p1, &mut p2); }
+#[derive(Debug)]
+pub struct Line(pub Vector2<i32>, pub Vector2<i32>);
 
-    let total_height = p2.y - p0.y;
+#[derive(Debug)]
+pub struct Box(pub Vector2<i32>, pub Vector2<i32>);
 
-    // render bottom part
-    let bottom_height = p1.y - p0.y;
-    for i in 0..bottom_height {
-        let total_height_fraction = i as f32 / total_height as f32;
-        let local_height_fraction = i as f32 / bottom_height as f32;
-        let mut from = (p0.x as f32 + (p2.x - p0.x) as f32 * total_height_fraction) as u32;
-        let mut to = (p0.x as f32 + (p1.x - p0.x) as f32 * local_height_fraction) as u32;
-        if from > to { mem::swap(&mut from, &mut to); }
-        for x in from..to {
-            img.put_pixel(x, (p0.y + i) as u32, pixel);
-        }
-    }
+impl IntoIterator for Box {
+    type Item = Vector2<i32>;
+    type IntoIter = BoxIterator;
 
-    // render top part
-    let top_height = p2.y - p1.y;
-    for i in 0..top_height {
-        let alpha = (i + bottom_height) as f32 / total_height as f32;
-        let beta = i as f32 / top_height as f32;
-        let mut from = (p0.x as f32 + (p2.x - p0.x) as f32 * alpha) as u32;
-        let mut to = (p1.x as f32 + (p2.x - p1.x) as f32 * beta) as u32;
-        if from > to { mem::swap(&mut from, &mut to); }
-        for x in from..to {
-            img.put_pixel(x, (p0.y + i + bottom_height) as u32, pixel);
+    fn into_iter(self) -> Self::IntoIter {
+        BoxIterator {
+            current: self.0,
+            bbox: self,
         }
     }
 }
 
+pub struct BoxIterator {
+    bbox: Box,
+    current: Vector2<i32>,
+}
+
+impl Iterator for BoxIterator {
+    type Item = Vector2<i32>;
+
+    fn next(&mut self) -> Option<Vector2<i32>> {
+        let x = self.current;
+        self.current.x += 1;
+        if self.current.x > self.bbox.1.x {
+            self.current.x = self.bbox.0.x;
+            self.current.y += 1;
+        };
+        if self.current.y > self.bbox.1.y {
+            return None;
+        }
+        Some(x)
+    }
+}
+
+impl Triangle {
+    fn bounding_box(&self) -> Box where {
+        let mut bboxmin = Vector2::new(i32::MAX, i32::MAX);
+        let mut bboxmax = Vector2::new(i32::MIN, i32::MIN);
+        for p in &[self.0, self.1, self.2] {
+            bboxmin.x = min(bboxmin.x, p.x);
+            bboxmax.x = max(bboxmax.x, p.x);
+            bboxmin.y = min(bboxmin.y, p.y);
+            bboxmax.y = max(bboxmax.y, p.y);
+        };
+        Box(bboxmin, bboxmax)
+    }
+
+    // triangle contains the point if all of the barycentric coordinates are positive or zero
+    fn contains(&self, point: Vector2<i32>) -> bool {
+        let coors = self.barycentric_coordinates(point);
+        !(coors.x < 0. || coors.y < 0. || coors.z < 0.)
+    }
+
+    // compute barycentric coordinates of point p with respect to the triangle
+    // https://en.wikipedia.org/wiki/Barycentric_coordinate_system
+    fn barycentric_coordinates(&self, p: Vector2<i32>) -> Vector3<f32> {
+        // vector AB_x, AC_x, and PA_x
+        let x = Vector3::new(self.2.x - self.0.x, self.1.x - self.0.x, self.0.x - p.x);
+        // vector AB_y, AC_y, and PA_y
+        let y = Vector3::new(self.2.y - self.0.y, self.1.y - self.0.y, self.0.y - p.y);
+        // find cross-product, i.e. orthogonal vector to both of the previous vectors
+        let u = x.cross(&y);
+        if u.z.abs() < 1 { return Vector3::new(-1., 1., 1.); }
+        // and finally calculate the barycentric coordinates
+        Vector3::new(1. - (u.x + u.y) as f32 / u.z as f32, u.y as f32 / u.z as f32, u.x as f32 / u.z as f32)
+    }
+}
+
+// TODO instead of u32 use generic
+fn draw_triangle<P: 'static + Pixel, Container: DerefMut<Target=[P::Subpixel]>>(mut t: Triangle, img: &mut ImageBuffer<P, Container>, pixel: P) {
+    let mut bbox = t.bounding_box();
+    if bbox.0.x < 0 { bbox.0.x = 0 };
+    if bbox.1.x > img.width() as i32 - 1 { bbox.1.x = img.width() as i32 - 1 };
+    if bbox.0.y < 0 { bbox.0.y = 0 };
+    if bbox.1.y > img.height() as i32 - 1 { bbox.1.x = img.height() as i32 - 1 };
+    for p in bbox.into_iter().filter(|&p| t.contains(p)) {
+        img.put_pixel(p.x as u32, p.y as u32, pixel);
+    }
+}
+
 // TODO: instead of u32 use generic
-fn draw_line<P: 'static + Pixel, Container: DerefMut<Target=[P::Subpixel]>>(mut x0: u32, mut y0: u32, mut x1: u32, mut y1: u32, img: &mut ImageBuffer<P, Container>, pixel: P) {
+fn draw_line<P: 'static + Pixel, Container: DerefMut<Target=[P::Subpixel]>>(mut l: Line, img: &mut ImageBuffer<P, Container>, pixel: P) {
     // transpose if the line is steep
     let mut steep = false;
-    if (x1 as i32 - x0 as i32).abs() < (y1 as i32 - y0 as i32).abs() {
-        mem::swap(&mut x0, &mut y0);
-        mem::swap(&mut x1, &mut y1);
+    if (l.1.x - l.0.x).abs() < (l.1.y - l.0.y).abs() {
+        let tmp = l.0.x;
+        l.0.x = l.0.y;
+        l.0.y = tmp;
+        let tmp = l.1.x;
+        l.1.x = l.1.y;
+        l.1.y = tmp;
         steep = true;
     }
     // left to right
-    if x0 > x1 {
-        mem::swap(&mut x0, &mut x1);
-        mem::swap(&mut y0, &mut y1);
+    if l.0.x > l.1.x {
+        mem::swap(&mut l.0, &mut l.1);
     }
-    let dx = x1 as i32 - x0 as i32;
-    let dy = y1 as i32 - y0 as i32;
+    let d = l.1 - l.0;
     // how much further from the real line will we get in each iteration
-    let derror2 = dy.abs() * 2;
+    let derror2 = d.y.abs() * 2;
     let mut error2 = 0;
-    let mut y = y0;
-    for x in x0..x1 {
+    let mut y = l.0.y;
+    for x in l.0.x..l.1.x {
         match steep {
-            true => img.put_pixel(y as u32, x, pixel),
-            false => img.put_pixel(x, y as u32, pixel)
+            true => img.put_pixel(y as u32, x as u32, pixel),
+            false => img.put_pixel(x as u32, y as u32, pixel)
         }
         error2 += derror2;
         if error2 > 1 {
-            if y1 > y0 { y += 1; } else { y -= 1; }
-            error2 -= dx * 2;
+            if l.1.y > l.0.y { y += 1; } else { y -= 1; }
+            error2 -= d.x * 2;
         }
     }
 }
@@ -90,17 +145,26 @@ fn render(source: &str, out: &str, width: u32, height: u32) {
         }
     }
 
-    let white = image::Rgb([255, 255, 255]);
     for face in (0..mesh.indices.len()).step_by(3) {
         let vertices = &mesh.indices[face..face + 3];
-        for i in 0..3 {
-            let v0 = &mesh.positions[(vertices[i] * 3) as usize..(vertices[i] * 3 + 3) as usize];
-            let v1 = &mesh.positions[(vertices[(i + 1) % 3] * 3) as usize..(vertices[(i + 1) % 3] * 3 + 3) as usize];
-            let x0 = (v0[0] + 1.) * width as f32 / 2. - 1.;
-            let y0 = (v0[1] + 1.) * height as f32 / 2. - 1.;
-            let x1 = (v1[0] + 1.) * width as f32 / 2. - 1.;
-            let y1 = (v1[1] + 1.) * height as f32 / 2. - 1.;
-            draw_line(x0 as u32, y0 as u32, x1 as u32, y1 as u32, &mut image, white);
+        let scale = |p: &[f32]| -> Vector2<i32> {
+            let x0 = (p[0] + 1.) * width as f32 / 2. - 1.;
+            let y0 = (p[1] + 1.) * height as f32 / 2. - 1.;
+            Vector2::new(x0 as i32, y0 as i32)
+        };
+        let get_point = |i| -> &[f32] {
+            &mesh.positions[(i * 3) as usize..(i * 3 + 3) as usize]
+        };
+        let as_vector = |p: &[f32]| -> Vector3<f32> {
+            Vector3::new(p[0], p[1], p[2])
+        };
+        let (x, y, z) = (as_vector(get_point(vertices[0])), as_vector(get_point(vertices[1])), as_vector(get_point(vertices[2])));
+        let light_dir = Vector3::new(0., 0., -1.);
+        let n = ((z - x).cross(&(y - x))).normalize();
+        let intensity = n.dot(&light_dir);
+        if intensity > 0. {
+            let t = Triangle(scale(get_point(vertices[0])), scale(get_point(vertices[1])), scale(get_point(vertices[2])));
+            draw_triangle(t, &mut image, image::Rgb([(intensity * 255.) as u8, (intensity * 255.) as u8, (intensity * 255.) as u8]));
         }
     }
     flip_vertical_in_place(&mut image);
@@ -108,18 +172,5 @@ fn render(source: &str, out: &str, width: u32, height: u32) {
 }
 
 fn main() {
-    // render("models/african_head.obj", "african_head.png", 600, 600);
-    let width = 200;
-    let height = 200;
-    let mut image: ImageBuffer<image::Rgb<u8>, Vec<u8>> = ImageBuffer::from_pixel(width, height, image::Rgb([0, 0, 0]));
-
-    let t0 = vec![Vector2::new(10, 70), Vector2::new(50, 160), Vector2::new(70, 80)];
-    let t1 = vec![Vector2::new(180, 50), Vector2::new(150, 1), Vector2::new(70, 180)];
-    let t2 = vec![Vector2::new(180, 150), Vector2::new(120, 160), Vector2::new(130, 180)];
-    draw_triangle(t0[0], t0[1], t0[2], &mut image, image::Rgb([255, 0, 0]));
-    draw_triangle(t1[0], t1[1], t1[2], &mut image, image::Rgb([255, 255, 255]));
-    draw_triangle(t2[0], t2[1], t2[2], &mut image, image::Rgb([0, 255, 0]));
-
-    flip_vertical_in_place(&mut image);
-    image.save(&Path::new("triangles.png")).unwrap();
+    render("models/african_head.obj", "african_head.png", 600, 600);
 }
